1c064391359329ae805ce909df5bf6b21af7d5da
Joe Gallagher <joedgallagher@wpi.edu>
2015-05-09 04:04:01 -0400

    MCV logic set up, main function created, initial screen created

--- BLCrawler/src/blcrawler/BLCrawler.java
+++ BLCrawler/src/blcrawler/BLCrawler.java
@@ -1,9 +1,27 @@
 package blcrawler;
 
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import blcrawler.controller.GUIMainController;
+import blcrawler.model.GUIModel;
+
 public class BLCrawler {
 
+	private static GUIMainController guiMainController;
+	private static GUIModel guiModel;
+	private static JLabel emptyLabel;
+	
+
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		guiModel = new GUIModel("gui");
+
+
+		
 
 	}
 
